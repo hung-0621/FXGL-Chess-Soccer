804a478edd6e514c91e23ab558ce52fcb0bc528b
凌薹安 <taian921215@gmail.com>
2025-04-29 19:28:21 +0800

    TestServer

---
+++ server/src/main/java/com/exp/server/ServerApplication.java
@@ -0,0 +1,16 @@
+package com.exp.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//mvn spring-boot:run
+//mvn clean install
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
