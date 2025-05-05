350da7513e54e491a814512c549aa82911f680e8
RzuzvTu <162456021+RzuzvTu@users.noreply.github.com>
2025-05-05 14:06:00 +0800

    Delete server directory

--- server/src/main/java/com/exp/server/repository/MatchRepository.java
+++
@@ -1,8 +0,0 @@
-package com.exp.server.repository;
-
-import com.exp.server.model.MatchModel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MatchRepository extends MongoRepository<MatchModel, String> {
-
-}
--- server/src/main/java/com/exp/server/repository/RoomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.exp.server.repository;
-
-import com.exp.server.model.RoomModel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RoomRepository extends MongoRepository<RoomModel, String> {
-    RoomModel findByRoomCode(String roomCode);
-}
